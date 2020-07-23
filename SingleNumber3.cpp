b920625fef6c7865ab2528a9876be0e7c229a68f
Jyotika Sharma <37184178+Jyotika25@users.noreply.github.com>
2020-07-23 18:09:46 +0530

    WEEK 3 PROBLEM 2

---
+++ SingleNumber3.cpp
@@ -0,0 +1,31 @@
+class Solution{
+public:
+  vector<int> singleNumber3(vector<int>& nums){
+  vector<int> output;
+  int n = nums.size();
+  int temp = 0;
+  int res1 =0;
+  int res2 =0;
+  
+  for(int i : nums)
+  {
+    temp = temp ^ i;
+  }
+  
+  int mask = 1;
+  
+  while((temp & mask) ==0){
+  mask = mask << 1;
+  }
+  
+  for(int i : nums){
+  if((mask & i) == 0)
+  res1 = res1 ^ i;
+  else
+  res2 = res2 ^ i;
+  }
+  output.push_back(res1);
+  output.push_back(res2);
+  return output;
+  }
+};
